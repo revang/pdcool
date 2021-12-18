ba03c56a18f2e548187628327b93138d1b0afd99
revang <revang.alternative@foxmail.com>
2021-12-18 20:38:24 +0800

    补全dataframe工具类

--- README.md
+++ README.md
@@ -3,44 +3,17 @@
 1. 获取基金成分股信息
 2. 获取股票涨跌幅信息（hsudata、tushare）
 3. 获取指数行情
-4. dataframe函数补充
-    - dataframe <---> json
-    - dataframe <---> csv
-    - dataframe <---> dict
-    - show_dataframe
-    - series ---> dataframe
-    - series <---> dict
-    - dataframe <---> excel
-5. 添加单元测试
+4. [OK]dataframe函数补充
+    - dataframe <---> json  ---> OK
+    - dataframe <---> csv   ---> OK
+    - show_dataframe        ---> OK
+    - series ---> dataframe ---> OK
+    - series <---> dict     ---> OK
+    - dataframe <---> excel ---> OK
+5. [OK]添加单元测试
 
 
 # 参考文档
 
 1. dataframe重命名: https://devnote.pro/posts/10000009321244
-2. 
-
-"""
-
-# import pandas as pd
-# import json
-# inp = [{'c1': 10, 'c2': 100}, {'c1': 11, 'c2': 110}, {'c1': 12, 'c2': 123}]
-# df = pd.DataFrame(inp)
-# print(df)
-# print("______________________________________________")
-# # -------------------------按行遍历iterrows():
-# for index, row in df.iterrows():
-#     print(index)  # 输出每行的索引值
-#     print(row['c1'], row['c2'])  # 输出每一行
-# print("______________________________________________")
-# # -------------------------按列遍历iteritems():
-# for index, row in df.iteritems():
-#     print(index)  # 输出列名
-#     print(row[0], row[1], row[2])  # 输出各列
-# print("______________________________________________")
-# json_text = df.to_json(orient="records")
-# json_data = json.loads(json_text)
-# for i in json_data:
-#     print(i)
-# # print(json_data)
-
-"""
+2. python文件结构: 
--- pdcool/utils/dataframe.py
+++ pdcool/utils/dataframe.py
@@ -1,33 +1,69 @@
+import json
 from pdcool.utils.config import dbconfig
 from sqlalchemy import create_engine
 import pandas as pd
+from pandas.core.series import Series
 
 
-# def dataframe_from_json(json_data):
-#     None
+def generate_simple_dataframe():
+    dict_list = [
+        {"name": "alice", "age": 18, "gender": "female"},
+        {"name": "bob", "age": 8, "gender": "male"},
+        {"name": "jack", "age": 13, "gender": "male"}
+    ]
+    df = pd.DataFrame(dict_list)
+    return df
+
+
+def dataframe_from_json(json_data):
+    """
+    加载json到dataframe. 注意: 这里json准确说应该是dict的list
+    """
+    df = pd.DataFrame(json_data)
+    return df
 
 
 def dataframe_to_json(df):
     """
-    加载dataframe到json
+    加载dataframe到json. 注意: 这里json准确说应该是dict的list
     """
     json_text = df.to_json(orient="records")
-    json_data = df.loads(json_text)
+    json_data = json.loads(json_text)
     return json_data
 
 
-# def dataframe_from_csv(csv):
-#     None
+def dataframe_to_csv(df, path):
+    """
+    加载dataframe到csv文件
+    """
+    df.to_csv(path, index=False)
 
 
-# def dataframe_to_csv(df, csv):
-#     None
+def dataframe_from_csv(path):
+    """
+    加载csv文件到dataframe
+    """
+    return pd.read_csv(path)
+
+
+def dataframe_to_excel(df, path):
+    """
+    加载dataframe到excel文件. 注意: 只支持xlsx格式
+    """
+    # df.to_csv(path, index=False)
+    df.to_excel(path, index=False)
+
+
+def dataframe_from_excel(path, sheet_name="Sheet1"):
+    """
+    加载excel文件到dataframe. 注意: 只支持xlsx格式
+    """
+    return pd.read_excel(path, sheet_name=sheet_name)
 
 
 def dataframe_to_table(dataframe, table, if_exists="append"):
     """
-    if_exists: fail 引发ValueError; replace 在插入新值前删除表; append 向现有表插入新值
-    加载dataframe到数据表
+    加载dataframe到数据表. if_exists: fail 引发ValueError; replace 在插入新值前删除表; append 向现有表插入新值
     """
     database_url = "mysql+pymysql://%s:%s@%s:%s/%s" % (dbconfig.get("username"), dbconfig.get("password"), dbconfig.get("host"), dbconfig.get("port"), dbconfig.get("database"))
     engine = create_engine(database_url)
@@ -47,8 +83,14 @@ def dataframe_from_sql(sql, type=None):
     return df
 
 
-# def show_dataframe(df, type="json"):
-#     None
+def show_dataframe(df, type="normal"):
+    if type == "normal":
+        print(df)
+    if type == "json":
+        json_data = dataframe_to_json(df)
+        for i in json_data:
+            print(i)
+    raise ValueError(f"invalid type: {type}")
 
 
 def dataframe_concat(df_list, type="row"):
@@ -59,4 +101,23 @@ def dataframe_concat(df_list, type="row"):
         return pd.concat(df_list)
     if type == "column":
         return pd.concat(df_list, axis=1)
-    raise ValueError("")
+    raise ValueError(f"invalid type: {type}")
+
+
+def generate_simple_series():
+    user_dict = {"name": "alice", "age": 18, "gender": "female"}
+    return pd.Series(user_dict)
+
+
+def series_to_list(s):
+    return Series.tolist(s)
+
+
+def series_to_dict(s):
+    return Series.to_dict(s)
+
+
+def series_to_dataframe(s):
+    df = pd.DataFrame(s)
+    df = df.T  # 转置: 交换行列
+    return df
