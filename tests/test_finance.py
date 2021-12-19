494c0199ec63073ab3b5d8f1e29675d96daf1c08
revang <revang.alternative@foxmail.com>
2021-12-19 12:20:24 +0800

    调整单元测试

--- tests/test_finance.py
+++ tests/test_finance.py
@@ -8,7 +8,13 @@ logging.basicConfig(level=logging.DEBUG)
 class FinanceTest(unittest.TestCase):
     def test_get_stock_name(self):
         name = get_stock_name("600570.SH")
-        print(name)
+        logging.debug(name)
+        assert name == "恒生电子"
+
+    def test_get_fund_name(self):
+        name = get_fund_name("320007.OF")
+        logging.debug(name)
+        assert name == "诺安成长"
 
 
 if __name__ == '__main__':
