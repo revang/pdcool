f40347a99c66a5d4ba9159dfeee12a98570e6798
revang <revang.alternative@foxmail.com>
2021-12-21 12:26:08 +0800

    调整表结构

--- database/pdm_table_tfinance_income.sql
+++ database/pdm_table_tfinance_income.sql
@@ -1,21 +1,24 @@
 create table tfinance_income
 (
-    c_user_code       varchar(20)  comment '用户代码' not null,
+    c_date            varchar(20)  comment '收益日期' not null,
     c_channel         varchar(20)  comment '销售渠道' not null,
+    c_user_code       varchar(20)  comment '用户代码' not null,
     c_fina_code       varchar(20)  comment '金融代码' not null,
     c_fina_type       varchar(20)  comment '金融类型' not null,
     n_cost            double(16,4) comment '成本',
     n_sell            double(16,4) comment '卖出金额',
-    n_share           double(16,4) comment '当日份额', 
+    n_share           double(16,4) comment '当前份额', 
     n_price           double(16,4) comment '当日价格',
     n_pre_price       double(16,4) comment '昨日价格',
-    n_today_cost      double(16,4) comment '当日新增成本',
-    n_today_sell      double(16,4) comment '当日新增卖出金额',
-    n_today_inc_share double(16,4) comment '当日买入份额',
-    n_today_dec_share double(16,4) comment '当日卖出份额',
+    n_today_cost      double(16,4) comment '当日成本(新增)',
+    n_today_sell      double(16,4) comment '当日卖出金额(新增)',
+    n_today_inc_share double(16,4) comment '当日买入份额(新增)',
+    n_today_dec_share double(16,4) comment '当日卖出份额(新增)',
+    n_today_income    double(16,4) comment '当日收益',
+    n_total_income    double(16,4) comment '累计收益',
     c_create_time     varchar(20)  comment '创建时间',
     c_update_time     varchar(20)  comment '修改时间',
-    primary key(c_user_code,c_channel,c_fina_code,c_fina_type)
+    primary key(c_date,c_channel,c_user_code,c_fina_code,c_fina_type)
 )
 engine=innodb default charset=utf8mb4;
 
